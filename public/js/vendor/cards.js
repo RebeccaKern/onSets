cbd9df96facd796d6cb14df9e4e39f1386bbdde1
Rebecca Kern <rkern@andrew.cmu.edu>
2016-12-05 14:07:32 -0500

    added card css

--- public/js/vendor/cards.js
+++ public/js/vendor/cards.js
@@ -3,6 +3,10 @@ function drawCards(){
     var cardsArray = new Array();
 
     cardsArray[0] = {src:'images/cards/G.png', id: '', class: ''};
+    cardsArray[1] = {src:'images/cards/BG.png', id: '', class: ''};
+    cardsArray[2] = {src:'images/cards/RGY.png', id: '', class: ''};
+    cardsArray[3] = {src:'images/cards/Blank.png', id: '', class: ''};
+    cardsArray[4] = {src:'images/cards/Y.png', id: '', class: ''};
 
     //then add them to the dom
     var c = document.getElementById('cards');
@@ -11,6 +15,7 @@ function drawCards(){
     {
         img = new Image();
         img.src = cardsArray[i].src;
+        img.className = "cardImg";
         c.append(img);
     }
 }
