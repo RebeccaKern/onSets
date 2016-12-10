bd58f8e9567b88154280b3bd934fd03a9f9d2446
Rebecca Kern <rkern@andrew.cmu.edu>
2016-12-10 13:47:33 -0500

    something is saved locally with manifest storage

---
+++ public/javascripts/startScreen.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+
+    console.log("in startscreen.js");
+
+    $(function() { 
+        console.log("in this function");
+        var i = loaditems();
+        $("#nameDisplay").html(i);
+        console.log("should be displaying name");
+    });
+
+    window.applicationCache.addEventListener("updateready",function(){
+        window.applicationCache.swapCache();
+        location.reload();
+    });
+
+    $("#submitButton").on("click", function(e){
+        console.log("submitButton clicked");
+        e.preventDefault();
+        saveName();
+    });
+
+    function saveName() {
+        var nameValue = document.getElementById("username").value;
+        var divTest = document.getElementById("nameDisplay");
+        $("#nameDisplay").html(nameValue);
+        saveitems(nameValue)
+    }
+
+    function saveitems(items) {
+        localStorage.items = JSON.stringify(items);
+    }
+
+    function loaditems() {
+        return JSON.parse(localStorage.items || "[]");
+    }
+
+}
