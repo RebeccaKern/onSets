c4994557971e5556dada861d12d69e9288f2b729
Rebecca Kern <rkern@andrew.cmu.edu>
2016-12-10 12:53:12 -0500

    removed a bunch of dead code

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -6,26 +6,12 @@ function setTimer(){
 
     function timer(){
       time=time-1;
-      //console.log(time);
       if (time <= 0)
       {
          clearInterval(counter);
          //counter ended, do something here
          return;
       }
-
-    // var socket = io.connect();
-    //   socket.on('connect', function () {
-    //     socket.emit('timing', function (data) {
-    //       console.log(data); // data will be 'woot'
-    //     });
-    //   });
-    // socket.emit('timing', time);
-    // console.log(time);
-
-    // socket.on('timing', function(message) {
-    //$('#time').html(time);
-    //console.log($('#time').html());
       var socket = io();
     socket.emit('evil', time);
     
--- socketio/serverSocket.js
+++ socketio/serverSocket.js
@@ -21,55 +21,6 @@ exports.init = function(io) {
          --currentPlayers;
          socket.broadcast.emit('players', { number: currentPlayers});
      });
-  		// socket.on('evil', function(finalT){
-    // 		io.emit('evil', finalT);
-    // 	});
 });
-// 	var currentPlayers = 0; // keep track of the number of players
-// 	var n = 0;
-// 	var myElement = document.getElementById("time");
-// 	console.log("once");
-// 	console.log(myElement);
-// // 	var countdown = 1000;  
-// // setInterval(function() {  
-// //   countdown--;
-// //   io.sockets.emit('timer', { countdown: countdown });
-// // }, 1000);
-//   // When a new connection is initiated
-// 	io.sockets.on('connection', function (socket) {
-// 		++currentPlayers;
-// 		++n;
-// 		// Send ("emit") a 'players' event back to the socket that just connected.
-// 		socket.emit('players', { number: currentPlayers, ordinal: n});
-// 		/*
-// 		 * Emit players events also to all (i.e. broadcast) other connected sockets.
-// 		 * Broadcast is not emitted back to the current (i.e. "this") connection
-//      */
-// 		socket.broadcast.emit('players', { number: currentPlayers, ordinal: n});
-// 		// socket.emit('timing', {number: time});
-// 		// socket.broadcast.emit('timing', {number: time});
-// 		// console.log("middle");
-// 		// socket.emit('t', {s: myElement});
-// 		// socket.broadcast.emit('t', {s: myElement});
 
-// 		// socket.on('chat message', function(msg){
-//   //   		io.emit('chat message', msg);
-//   // 		});
-
-
-// 	  // socket.on('reset', function (data) {
-// 	  //   countdown = 1000;
-// 	  //   io.sockets.emit('timer', { countdown: countdown });
-// 	  // });
-// 		/*
-// 		 * Upon this connection disconnecting (sending a disconnect event)
-// 		 * decrement the number of players and emit an event to all other
-// 		 * sockets.  Notice it would be nonsensical to emit the event back to the
-// 		 * disconnected socket.
-// 		 */
-// 		socket.on('disconnect', function () {
-// 			--currentPlayers;
-// 			socket.broadcast.emit('players', { number: currentPlayers});
-// 		});
-// 	});
 }
